0518145075d9a89e04d5accbe9c1c1bccac2a4a0
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/WindowsApplication53/MyPivotGrid.vb
@@ -0,0 +1,59 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Text
+Imports DevExpress.XtraPivotGrid
+Imports DevExpress.XtraPivotGrid.Data
+Imports DevExpress.XtraPivotGrid.ViewInfo
+Imports DevExpress.XtraEditors.Filtering
+Imports DevExpress.XtraEditors.Repository
+
+Namespace WindowsApplication53
+    Public Class MyPivotGridControl
+        Inherits PivotGridControl
+
+        Public Sub New()
+            MyBase.New()
+        End Sub
+        Public Sub New(ByVal viewInfoData As PivotGridViewInfoData)
+            MyBase.New(viewInfoData)
+        End Sub
+
+        Protected Overrides Function CreateData() As PivotGridViewInfoData
+            Return New MyPivotGridViewInfoData(Me)
+        End Function
+    End Class
+
+    Public Class MyPivotGridViewInfoData
+        Inherits PivotGridViewInfoData
+        Implements IFilteredComponent
+
+        Public Sub New()
+            MyBase.New()
+        End Sub
+        Public Sub New(ByVal control As IViewInfoControl)
+            MyBase.New(control)
+        End Sub
+
+
+        #Region "IFilteredComponent Members"
+
+        Private Function IFilteredComponent_CreateFilterColumnCollection() As IBoundPropertyCollection Implements IFilteredComponent.CreateFilterColumnCollection
+            Dim collection As IBoundPropertyCollection = New FieldFilterColumnCollection(Fields, Prefilter.PrefilterColumnNames, MenuManager)
+            For Each column As FieldFilterColumn In collection
+                UpdateFieldFilterColumn(column)
+            Next column
+            Return collection
+        End Function
+
+        Private Sub UpdateFieldFilterColumn(ByVal filterColumn As FieldFilterColumn)
+            Dim comboBox As RepositoryItemComboBox = TryCast(filterColumn.ColumnEditor, RepositoryItemComboBox)
+            If comboBox IsNot Nothing Then
+                comboBox.AutoComplete = False
+            End If
+        End Sub
+        #End Region
+
+
+    End Class
+
+End Namespace
