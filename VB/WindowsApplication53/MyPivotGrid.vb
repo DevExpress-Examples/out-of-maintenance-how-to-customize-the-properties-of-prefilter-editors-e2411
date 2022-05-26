2254e7957e2070c3e2a3bbb7ed5642d1dc4b1d76
DevExpressExampleBot <fakeEmail>
2022-05-26 12:58:30 +0400

    Source auto update [skip ci]

--- VB/WindowsApplication53/MyPivotGrid.vb
+++ VB/WindowsApplication53/MyPivotGrid.vb
@@ -1,6 +1,3 @@
-﻿Imports System
-Imports System.Collections.Generic
-Imports System.Text
 Imports DevExpress.XtraPivotGrid
 Imports DevExpress.XtraPivotGrid.Data
 Imports DevExpress.XtraPivotGrid.ViewInfo
@@ -8,12 +5,14 @@ Imports DevExpress.XtraEditors.Filtering
 Imports DevExpress.XtraEditors.Repository
 
 Namespace WindowsApplication53
+
     Public Class MyPivotGridControl
         Inherits PivotGridControl
 
         Public Sub New()
             MyBase.New()
         End Sub
+
         Public Sub New(ByVal viewInfoData As PivotGridViewInfoData)
             MyBase.New(viewInfoData)
         End Sub
@@ -30,18 +29,18 @@ Namespace WindowsApplication53
         Public Sub New()
             MyBase.New()
         End Sub
+
         Public Sub New(ByVal control As IViewInfoControl)
             MyBase.New(control)
         End Sub
 
-
-        #Region "IFilteredComponent Members"
-
-        Private Function IFilteredComponent_CreateFilterColumnCollection() As IBoundPropertyCollection Implements IFilteredComponent.CreateFilterColumnCollection
+#Region "IFilteredComponent Members"
+        Private Overloads Function CreateFilterColumnCollection() As IBoundPropertyCollection Implements IFilteredComponent.CreateFilterColumnCollection
             Dim collection As IBoundPropertyCollection = New FieldFilterColumnCollection(Fields, Prefilter.PrefilterColumnNames, MenuManager)
             For Each column As FieldFilterColumn In collection
                 UpdateFieldFilterColumn(column)
-            Next column
+            Next
+
             Return collection
         End Function
 
@@ -51,9 +50,6 @@ Namespace WindowsApplication53
                 comboBox.AutoComplete = False
             End If
         End Sub
-        #End Region
-
-
+#End Region
     End Class
-
 End Namespace
